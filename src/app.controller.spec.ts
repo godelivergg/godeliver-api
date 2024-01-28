fd33b0d4fe62a76081b69a01aa95d2b2b03d2d27
Thayane Bomfim <thayanesilbomfim@outlook.com>
2024-01-28 20:39:59 -0300

    feat: insert security in temporary controller

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { JwtAuthGuard } from './auth/jwtAuth.guard';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -9,7 +10,11 @@ describe('AppController', () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
-    }).compile();
+    })
+      .overrideGuard(JwtAuthGuard)
+      .useValue({
+        canActivate: jest.fn().mockReturnValue(true),
+      }).compile();
 
     appController = app.get<AppController>(AppController);
   });
