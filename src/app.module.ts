ae872fb4db54939ddc369397dc09ab8d5e0de060
Thayane Bomfim <thayanesilbomfim@outlook.com>
2024-01-29 19:28:27 -0300

    feat: add user module to main module and typeorm configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoggerModule } from './helpers/logger/logger.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { UserModule } from './app/user/user.module';
 
 @Module({
   imports: [
@@ -11,7 +13,23 @@ import { LoggerModule } from './helpers/logger/logger.module';
     JwtModule.register({
       secret: process.env.JWT_SECRET_KEY
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+      ({
+        type: process.env.TYPEORM_CONNECTION,
+        host: configService.get('POSTGRES_HOST', 'localhost'),
+        port: Number(configService.get('POSTGRES_PORT', 5432)),
+        username: configService.get('POSTGRES_USER', ''),
+        password: configService.get('POSTGRES_PASSWORD', ''),
+        database: configService.get('POSTGRES_DATABASE', ''),
+        entities: [__dirname + '/**/*.entity{.js,.ts}'],
+        synchronize: true,
+      } as TypeOrmModuleOptions),
+    }),
     LoggerModule,
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
